b5aa8e31d321027cccf0f21895c1a843b490b10e
ar135 <archat71@gmail.com>
2021-07-26 16:19:49 -0400

    changes

---
+++ data.js
@@ -0,0 +1,33 @@
+const questions = [
+  {
+    id: 1,
+    title: 'How is this product?',
+    info:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id ornare arcu odio ut sem nulla pharetra diam. Rhoncus urna neque viverra justo. Malesuada fames ac turpis egestas integer. Rhoncus mattis rhoncus urna neque viverra justo. Sem fringilla ut morbi tincidunt augue interdum velit euismod. Faucibus ornare suspendisse sed nisi lacus sed viverra tellus in. Maecenas ultricies mi eget mauris pharetra et.',
+  },
+  {
+    id: 2,
+    title: 'What are the features of this product?',
+    info:
+      'Est sit amet facilisis magna etiam tempor orci eu lobortis. Sapien pellentesque habitant morbi tristique senectus et netus et. Amet mauris commodo quis imperdiet massa tincidunt nunc pulvinar. Etiam dignissim diam quis enim lobortis scelerisque fermentum dui faucibus. Ullamcorper morbi tincidunt ornare massa eget egestas purus. Eget velit aliquet sagittis id. Pulvinar neque laoreet suspendisse interdum. Arcu dui vivamus arcu felis bibendum ut tristique.',
+  },
+  {
+    id: 3,
+    title: 'How will this product help me?',
+    info:
+      'Et malesuada fames ac turpis egestas integer eget. Leo in vitae turpis massa. Eget nunc scelerisque viverra mauris in aliquam sem fringilla ut. Cursus in hac habitasse platea dictumst quisque sagittis purus sit. Habitasse platea dictumst quisque sagittis. Mi bibendum neque egestas congue quisque.',
+  },
+  {
+    id: 4,
+    title: 'How does this product change from other products?',
+    info:
+      'Ultrices neque ornare aenean euismod elementum nisi. Feugiat in fermentum posuere urna nec tincidunt. Faucibus vitae aliquet nec ullamcorper sit. Neque laoreet suspendisse interdum consectetur libero id faucibus. Feugiat nibh sed pulvinar proin gravida. Vitae tempus quam pellentesque nec. Urna nunc id cursus metus aliquam. Dignissim cras tincidunt lobortis feugiat vivamus at augue.',
+  },
+  {
+    id: 5,
+    title: 'What features of this product are the most creative?',
+    info:
+      'Egestas purus viverra accumsan in nisl. Faucibus purus in massa tempor. Scelerisque mauris pellentesque pulvinar pellentesque. Gravida neque convallis a cras semper auctor neque vitae tempus. Ullamcorper dignissim cras tincidunt lobortis. Pretium vulputate sapien nec sagittis. Aliquet porttitor lacus luctus accumsan tortor. Condimentum mattis pellentesque id nibh tortor id aliquet lectus. Neque aliquam vestibulum morbi blandit cursus risus at ultrices. Ultricies leo integer malesuada nunc. Eget nullam non nisi est sit amet facilisis magna etiam.',
+  },
+]
+export default questions
